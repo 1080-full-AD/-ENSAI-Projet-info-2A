21d770b44ced7dc01834b1cbcafec3446d2663b9
Adrien Gibert <gibert_adrien@hotmail.fr>
2024-10-11 12:38:40 +0200

    fezfe

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import requests
-
-r = requests.get('https://api.jikan.moe/v4/manga')
-r.status_code
-print(r.json())
+for i in range(1, 2000):
+    r = requests.get('https://api.jikan.moe/v4/manga', {"page" :1})
+    r.status_code
+    print(r.json())
 
