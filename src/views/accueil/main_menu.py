a50837be9ce2fcfe1f68446bea02e67a080b0ecd
Adrien Gibert <gibert_adrien@hotmail.fr>
2024-10-21 16:35:53 +0200

    creation des views

---
+++ src/views/accueil/main_menu.py
@@ -0,0 +1,48 @@
+from InquirerPy import inquirer
+
+from src.utils.reset_database import ResetDatabase
+
+from src.views.vue_abstraite import AbstractView
+from src.views.session import Session
+
+
+class MainView(AbstractView):
+    """Vue d'accueil de l'application"""
+
+    def choisir_menu(self):
+        """Choix du menu suivant
+
+        Return
+        ------
+        view
+            Retourne la vue choisie par l'utilisateur dans le terminal
+        """
+
+        print("\n" + "-" * 50 + "\Bienvenue :)\n" + "-" * 50 + "\n")
+
+        choix = inquirer.select(
+            message="Faites votre choix : ",
+            choices=[
+                "Se connecter",
+                "Créer un compte",
+                "Rechercher des mangas",
+                "Quitter",
+            ],
+        ).execute()
+
+        match choix:
+            case "Quitter":
+                pass
+
+            case "Se connecter":
+                from src.views.accueil.connexion_vue import ConnexionVue
+
+                return ConnexionVue("Connexion à l'application")
+
+            case "Créer un compte":
+                from src.views.accueil.inscription_vue import InscriptionVue
+
+                return InscriptionVue("Création de compte joueur")
+
+            case "Rechercher des mangas":
+                from src.views.manga_search import MangaSearch
