26d0ff1383da2070dffb6825e172a957362d0a09
id2354 <jeanne.madec@eleve.ensai.fr>
2024-10-23 12:32:39 +0200

    tests utilisateurs

--- src/tests/test_service/test_utilisateur_service.py
+++ src/tests/test_service/test_utilisateur_service.py
@@ -5,14 +5,13 @@ from src.dao.utilisateur_dao import UtilisateurDao
 from src.business_objet.utilisateur import Utilisateur
 
 # Exemple d'utilisateur: 
-# "liste_utilisateur
-[
+liste_utilisateur=[
 Utilisateur(pseudo="Naruto54", age=16, mdp=("mdpManga4#", "Naruto54"), collections=["Favoris"], id_utilisateur=1),
 Utilisateur(pseudo="missmanga", age=18, mdp=("126OnePiece#", "missmanga"), collections=["Favoris"], id_utilisateur=2),
 Utilisateur(pseudo="23One", age=22, mdp=("78Naruto#", "23One"), collections=["Favoris"], id_utilisateur=5),
 ]
 
-def pseudo_existe_deja_ok():
+def test_pseudo_existe_deja_ok():
     """Vérifier que la méthode pseudo_existe_déjà fonctionne correctement"""
 
     # GIVEN
@@ -30,7 +29,7 @@ def pseudo_existe_deja_ok():
     assert utilisateur is True
 
 
-def pseudo_existe_déjà_echec():
+def test_pseudo_existe_déjà_echec():
     """Vérifier que la méthode pseudo_existe_déjà renvoie bien une erreur"""
 
     # GIVEN
@@ -48,7 +47,7 @@ def pseudo_existe_déjà_echec():
     assert utilisateur is False
 
 
-def creer_utilisateur_ok():
+def test_creer_utilisateur_ok():
     """Vérifier que la méthode creer_utilisateur fonctionne bien"""
 
     # GIVEN
@@ -66,7 +65,7 @@ def creer_utilisateur_ok():
     assert utilisateur is True
 
 
-def creer_utilisateur_echec():
+def test_creer_utilisateur_echec():
     """Vérifier que la méthode creer_utilisateur renvoie bien un échec"""
 
     # GIVEN
@@ -84,12 +83,161 @@ def creer_utilisateur_echec():
     assert utilisateur is False
 
 
-def modifier_utilisateur_ok():
+def test_modifier_utilisateur_ok():
     """Vérifier que la méthode modifier_utilisateur fonctionne correctement"""
 
     # GIVEN
+    pseudo, age, mdp, collections, id_utilisateur = "fanmang", 22, "Monste_R12", ["Monster", "One Piece", "Naruto"], 678
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.modifier_utilisateur.return_value = True
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    utilisateur = utilisateur_service.modifier_utilisateur(pseudo, age, mdp, collections, id_utilisateur )
+
+    # THEN
+    assert utilisateur is True
+
+
+def test_modifier_utilisateur_echec():
+    """Vérifier que la méthode modifier_utilisateur renvoie bien une erreur"""
+
+    # GIVEN
+    pseudo, age, mdp, collections, id_utilisateur = "fanmang", 22, "Monste_R12", ["Monster", "One Piece", "Naruto"], 678
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.modifier_utilisateur.return_value = False
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    utilisateur = utilisateur_service.modifier_utilisateur(pseudo, age, mdp, collections, id_utilisateur )
+
+    # THEN
+    assert utilisateur is False
+
+
+def test_supprimer_utilisateur_ok():
+    """Vérifier que la méthode supprimer_utilsiateur fonctionne correctement"""
+
+    # GIVEN
+    pseudo, age, mdp, collections, id_utilisateur = "fanmang", 22, "Monste_R12", ["Monster", "One Piece", "Naruto"], 678
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.supprimer_utilisateur.return_value = True
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    utilisateur = utilisateur_service.supprimer_utilisateur(pseudo, age, mdp, collections, id_utilisateur )
+
+    # THEN
+    assert utilisateur is True
+
+
+def test_supprimer_utilisateur_echec():
+    """Vérifier que la méthode supprimer_utilsiateur renvoie bien une erreur"""
+
+    # GIVEN
+    pseudo, age, mdp, collections, id_utilisateur = "fanmang", 22, "Monste_R12", ["Monster", "One Piece", "Naruto"], 678
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.supprimer_utilisateur.return_value = False
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    utilisateur = utilisateur_service.supprimer_utilisateur(pseudo, age, mdp, collections, id_utilisateur )
+
+    # THEN
+    assert utilisateur is False
+
+
+def test_trouver_par_pseudo_utilisateur_ok():
+    """Vérifier que la méthode qui liste tous les utilisateurs grâce à leur pseudo fonctionne"""
+
+    # GIVEN
+    pseudo = pseudo.utilisateur_service()
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.trouver_tous_utilisateur.return_value = [pseudo for pseudo in liste_utilisateur if pseudo.id_utilisateur == pseudo]
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    res = utilisateur_service.trouver_tous_utilisateur(pseudo)
+
+    # THEN
+    assert len(res) == 2
+    for pseudo in res:
+        assert pseudo.id_utilisateur == pseudo
+
+
+def test_trouver_par_pseudo_utilisateur_echec():
+    """Vérifier que la méthode qui renvoie tous les utilisateurs renvoie bien un échec"""
+
+    # GIVEN
+    pseudo = pseudo.utilisateur_service()
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.trouver_tous_utilisateur.side_effect = Exception("Erreur de base de données")
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    res = utilisateur_service.trouver_tous_utilisateur(pseudo)
+
+    # THEN
+    assert res == []
+
+
+def test_lister_tous_utilisateur_ok():
+    """Lister tous les utilisateurs avec succès"""
+
+    # GIVEN
+    id_utilisateur = id_utilisateur.utilisateur_service()
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.lister_tous_utilisateur.return_value = [pseudo for pseudo in liste_utilisateur if pseudo.id_utilisateur == id_utilisateur]
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    res = utilisateur_service.lister_tous_utilisateur(id_utilisateur)
+
+    # THEN
+    assert len(res) == 2
+    for pseudo in res:
+        assert pseudo.id_utilisateur == id_utilisateur
+
+
+def test_lister_tous_utilisateur_echec():
+    """Vérifier que la méthode qui liste tous les utilisateurs grâce à leur identifiant renvoie bien un échec"""
+
+    # GIVEN
+    id_utilisateur = id_utilisateur.utilisateur_service()
+    mock_dao = MagicMock(spec=UtilisateurDao)
+    mock_dao.lister_tous_utilisateur.side_effect = Exception("Erreur de base de données")
+
+    utilisateur_service = UtilisateurService()
+    utilisateur_service.UtilisateurDao = mock_dao 
+
+    # WHEN
+    res = utilisateur_service.lister_tous_utilisateur(id_utilisateur)
+
+    # THEN
+    assert res == []
+
+
+def test_se_connecter_ok():
     
-#test de test
+
+
+
+
+
 if __name__ == "__main__":
     import pytest
 
