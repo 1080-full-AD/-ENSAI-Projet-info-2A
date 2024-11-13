6634fd3fe3a7ed3240129822ee4729089631996a
RUTH2012-web <kambouheryruth@gmail.com>
2024-11-14 00:39:07 +0100

    modif

--- src/Business_objet/manga_physique.py
+++ src/Business_objet/manga_physique.py
@@ -15,38 +15,6 @@ class MangaPhysique(Manga):
         liste_tome = [i for i in range(1,self.dernier_tome+1) if i not in self.tomes_manquants]
         return liste_tome
 
-    def ajouter_tome(self, new_tome):
-        if not isinstance(new_tome, int):
-            raise TypeError("Le tome ajouté doit être un entier")
-        if new_tome in self.tomes_manquants:
-            self.tomes_manquants.remove(new_tome)
+    
 
-        elif new_tome > self.dernier_tome:
-            for i in range(1, new_tome-self.dernier_tome):
-                self.tomes_manquants.append(self.dernier_tome+i)
-            self.dernier_tome = new_tome
-
-        else:
-            print("tome deja existant")
-
-    def enlever_tome(self, tome):
-        if not isinstance(tome, int):
-            raise TypeError(f"{tome},n'est pas un tome")
-
-        if tome == self.dernier_tome:
-            self.tomes_manquants.append(tome)
-            self.tomes_manquants.sort()  # Trier pour garantir l'ordre croissant
-            
-        # On cherche le nouveau dernier tome qui n'est pas manquant
-            nouveau_dernier_tome = tome - 1
-            while nouveau_dernier_tome in self.tomes_manquants and nouveau_dernier_tome > 0:
-                nouveau_dernier_tome -= 1
-            
-            self.dernier_tome = nouveau_dernier_tome
-            
-        elif tome < self.dernier_tome:
-            self.tomes_manquants.append(tome)
-        else:
-            print("tome pas existant")
-
-
+    
---
+++ src/service/manga_physique_service.py
@@ -0,0 +1,65 @@
+from src.utils.singleton import Singleton
+from src.business_objet.manga_physique import MangaPhysique
+from src.dao.manga_physique_dao import MangaPhysiqueDao
+from src.utils.log_decorator import log
+
+
+class MangaService(metaclass=Singleton):
+    """Classe permettant d'avoir des informations à propos des Mangas"""
+
+
+    @log
+    def creer_manga_physique(self, manga) -> bool:
+        """Créer un manga physique dans la base de données"""
+        return MangaPhysiqueDao().creer(manga)
+
+    @log
+    def supprimer_manga_physique(self, manga) -> bool:
+        """Supprimer un manga de la base de données"""
+        return MangaPhysiqueDao().supprimer_manga_physique(manga)
+
+
+    def ajouter_tome(self, manga,new_tome):
+        """
+        ajouter un nouveau tome au manga physique
+
+        parameters
+        manga:manga 
+        new_tome:tome à ajouter 
+
+        return
+        """
+        if not isinstance(new_tome, int):
+            raise TypeError("Le tome ajouté doit être un entier")
+        if new_tome in manga.tomes_manquants:
+            manga.tomes_manquants.remove(new_tome)
+            return MangaPysiqueDao().modifier_manga_physique(manga)
+        elif new_tome > manga.dernier_tome:
+            for i in range(1, new_tome-manga.dernier_tome):
+                manga.tomes_manquants.append(manga.dernier_tome+i)
+                manga.dernier_tome = new_tome
+                return MangaPysiqueDao().modifier_manga_physique(manga)
+        else:
+            print("tome deja existant")
+
+        
+    def enlever_tome(manga, tome):
+        if not isinstance(tome, int):
+            raise TypeError(f"{tome},doit être un entier")
+
+        if tome == manga.dernier_tome:
+            manga.tomes_manquants.append(tome)
+            manga.tomes_manquants.sort()  # Trier pour garantir l'ordre croissant
+            
+        # On cherche le nouveau dernier tome qui n'est pas manquant
+            nouveau_dernier_tome = tome - 1
+            while nouveau_dernier_tome in manga.tomes_manquants and nouveau_dernier_tome > 0:
+                nouveau_dernier_tome -= 1
+                manga.dernier_tome = nouveau_dernier_tome
+            return MangaPysiqueDao().modifier_manga_physique(manga)
+        elif tome < manga.dernier_tome:
+            manga.tomes_manquants.append(tome)
+            return MangaPysiqueDao().modifier_manga_physique(manga)
+        else:
+            print("tome pas existant")
+
