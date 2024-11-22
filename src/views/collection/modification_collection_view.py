b1c0c62dfe0214365d5373f636f2b9acb230df28
Adrien Gibert <gibert_adrien@hotmail.fr>
2024-11-22 13:56:28 +0100

    aaa

--- src/views/collection/modification_collection_view.py
+++ src/views/collection/modification_collection_view.py
@@ -38,7 +38,7 @@ class ModificationCollectionView(AbstractView):
             message="Faites votre choix : ",
             choices=[
                 "Modifier le titre",
-                "Modifier la dscription",
+                "Modifier la description",
                 "Ajouter un manga",
                 "Enlever un manga",
                 "Retour",
@@ -50,47 +50,43 @@ class ModificationCollectionView(AbstractView):
                 titre = inquirer.text(
                     "Rédigez votre nouveau titre :)"
                 ).execute()
-                collection.titre = titre
                 try:
-                    CollectionVirtuelleService().modifier_collection(collection=collection)
+                    CollectionVirtuelleService().modifier_titre(collection=collection, new_titre=titre)
                 except Exception as e:
                     print("\n", e)
 
-                return  ModificationCollectionView(("\n" + "=" * 50 + " Modification de collection"
+                return ModificationCollectionView(("\n" + "=" * 50 + " Modification de collection"
                                         " :) " + "=" * 50 + "\n"))
 
-            
-            case "Modifier la dscription":
+            case "Modifier la description":
+                desc = inquirer.text(
+                    "Rédigez votre nouvelle description :)"
+                ).execute()
+                try:
+                    CollectionVirtuelleService().modifier_description(collection=collection, new_description=desc)
+                except Exception as e:
+                    print("\n", e)
 
-                note = int(inquirer.number(
-                    f"Donnez votre nouvelle note à {manga.titre_manga} :)",
-                    min_allowed=0,
-                    max_allowed=5
-                ).execute())
-                AvisService().noter(id_manga=id_manga,
-                                    id_utilisateur=id_utilisateur, note=note)
+                return ModificationCollectionView(("\n" + "=" * 50 + " Modification de collection"
+                                        " :) " + "=" * 50 + "\n"))
 
-                from src.views.users.main_opinion_view import MainOpinionView
-
-                return MainOpinionView("\n" + "=" * 50 + " Menu des avis"
-                                       " :) " + "=" * 50 + "\n")
-
-            case "Ajouter un manga a NOM COLLECTION":
+            case "Ajouter un manga":
                 
-                note = int(inquirer.number(
-                    f"Donnez votre nouvelle note à {manga.titre_manga} :)",
-                    min_allowed=0,
-                    max_allowed=5
-                ).execute())
-                AvisService().noter(id_manga=id_manga,
-                                    id_utilisateur=id_utilisateur, note=note)
+                titre_manga = inquirer.text(
+                    "Entrez le nom du manga que vous voulez ajouter :)"
+                ).execute()
+                try:
+                    manga = MangaService().rechercher_un_manga(
+                        titre_manga=titre_manga
+                        )
+                    CollectionVirtuelleService().ajouter_manga(collection=collection, new_manga=manga)
+                except Exception as e:
+                    print("\n", e)
 
-                from src.views.users.main_opinion_view import MainOpinionView
+                return ModificationCollectionView(("\n" + "=" * 50 + " Modification de collection"
+                                        " :) " + "=" * 50 + "\n"))
 
-                return MainOpinionView("\n" + "=" * 50 + " Menu des avis"
-                                       " :) " + "=" * 50 + "\n")
-
-            case "Enlever un manga a NOM COLLECTION":
+            case "Enlever un manga":
                 
                 note = int(inquirer.number(
                     f"Donnez votre nouvelle note à {manga.titre_manga} :)",
