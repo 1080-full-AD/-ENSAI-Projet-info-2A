64c47ad3eca196908d88cfce0e5c9a6208f842a2
id2354 <jeanne.madec@eleve.ensai.fr>
2024-11-23 10:06:01 +0100

    modif erreur niveau mangadao (créer manga)

--- src/dao/manga_dao.py
+++ src/dao/manga_dao.py
@@ -64,7 +64,8 @@ class MangaDao(metaclass=Singleton):
                     cursor.execute(
                         "INSERT INTO manga(id_manga, titre_manga, auteurs, synopsis, nb_volumes, nb_chapitres)"
                         "VALUES                                              "
-                        f"('{manga.id_manga}', '{manga.titre_manga}', '{manga.auteurs}', '{manga.synopsis}', '{manga.nb_volumes}', '{manga.nb_chapitres}') "
+                        f"('{manga.id_manga}', '{manga.titre_manga}', '{manga.auteurs}', '{manga.synopsis}',"
+                        f"'{manga.nb_volumes}', '{manga.nb_chapitres}')"
                         "  RETURNING id_manga;                               ",
                         {
                             "id_manga": manga.id_manga,
